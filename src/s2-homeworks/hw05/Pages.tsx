8cfac2f958db89ff85079003593a801798b314fe
vadimcrmnl1 <you@example.com>
2022-11-09 15:00:46 +0300

    hws5 - 091122 - 2

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -18,7 +18,7 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-
+                <Route path='/' element={<PreJunior/>}/>
                 <Route path='/pre-junior' element={<PreJunior/>}/>
                 <Route path='/junior' element={<Junior/>}/>
                 <Route path='/junior-plus' element={<JuniorPlus/>}/>
